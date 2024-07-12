113460129a12be6184313e39d16bc60f53e4e004
duruis <ruis.heduardo@gmail.com>
2024-07-12 16:33:26 -0300

    01

---
+++ learn_01.py
@@ -0,0 +1,33 @@
+# Immutable = you cannot change once you definite it 
+# str; int; float; bool; bytes; tuple 
+
+
+# Muttable = you can change them
+ # list []; set; dict
+ 
+x = [i for i in range (10)]
+y = [i for i in range (10) if i % 2 == 0]
+print(x + y)
+
+def function (x, y, z = 10):
+ print(x + y * z)
+ pass
+function (4, 6, )
+
+# args and kwargs will run as many args u want and different types
+def function (*args, **kwargs ): 
+ print(args, kwargs)
+ pass
+function (10, 34, 45, 90, c = true, d = 10)
+
+# using * and ** 
+def function ( a, b, c= True, d= False):
+  print(a, b, c, d)
+  pass
+function (*[1, 2], **{"c":  "cool", "d": "nice"})
+
+ 
+ 
+  
+
+
