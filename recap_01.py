4a31eea0fdb261aedaac889ace81eeb3a9a8038f
duruis <ruis.heduardo@gmail.com>
2024-07-21 21:24:46 -0300

    Jul_21

---
+++ recap_01.py
@@ -0,0 +1,35 @@
+#week_01
+'''
+names = input("What is your name? ").title().strip()
+first, last = names.split(" ")
+print(f"What's up {last}")
+'''
+'''
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+print(f"Sum of x + y = {x + y:,}")
+
+'''
+def main ():
+  x = int(input("What is x? "))
+  print("x square is", square(x))
+
+def square(n):
+  return pow(n, 2)
+  
+main()
+
+  
+'''
+def main ():
+ name = input("What is your name: ")
+ hello(name)
+ 
+def hello (sec="Du"):
+  print("What's up bruddah man, ", sec)
+hello ()
+
+main()
+'''
+
+
